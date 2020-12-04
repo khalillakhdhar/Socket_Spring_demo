22851b742d123f9801a9e18952ea5dae15a4b3ce
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-04 15:18:01 +0100

    Initial commit

---
+++ src/main/java/com/orsys/SocketSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.orsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketSpringDemoApplication.class, args);
+	}
+
+}
