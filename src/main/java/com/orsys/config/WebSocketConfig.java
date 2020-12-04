e435cf0ebf2ec6a8d4801da833ee5eaad7193d9d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-04 16:21:03 +0100

    Update WebSocketConfig.java

--- src/main/java/com/orsys/config/WebSocketConfig.java
+++ src/main/java/com/orsys/config/WebSocketConfig.java
@@ -1,5 +1,30 @@
 package com.orsys.config;
 
-public class WebSocketConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-}
+import com.orsys.interceptor.HttpHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+     
+    @Autowired
+    private HttpHandshakeInterceptor handshakeInterceptor;
+ 
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS().setInterceptors(handshakeInterceptor);
+    }
+ 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+       }
+    
+   }
